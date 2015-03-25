eb95905dc3a7947bd3187c1c728ad4fda12db9e8
Gabe Finch and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-03-25 15:41:46 -0700

    Add ability for user to list a space

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -2,4 +2,30 @@ class SpacesController<ApplicationController
   def index
     @spaces = Space.all
   end
+
+  def new
+    @space = Space.new
+  end
+
+  def create
+    @space = Space.new(space_params)
+    @space.user = current_user
+binding.pry
+    if @space.save
+      flash[:notice] = "Successfully created your space."
+      redirect_to space_path(@space)
+    else
+      flash[:alert] = "There were problems with your space. Try again."
+      redirect_to :back
+    end
+  end
+
+  def show
+    @space = Space.find(params[:id])
+  end
+
+private
+  def space_params
+    params.require(:space).permit(:address,:description,:cost)
+  end
 end
