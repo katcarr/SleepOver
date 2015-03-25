dd51ed5ac3f1bbcde4e6ef76e064e1bda0880546
Gabe Finch and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-03-25 15:02:39 -0700

    login/logout devise.views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
-  def new
-    
+  # def new
+  #   @user
+  # end
+  #
+  # def create
+  #   @user = User.new(user_params)
+  #   if @user.save
+  #     flash(:notice) = "Login successful"
+  #     redirect_to spaces_path
+  #   else
+  #     flash(:alert) = "There was problem with creating your account"
+  #     redirect_to :back
+  #   end
+  # end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
 end
