887897f084d572daacb20c95b050a46f549cb7b5
Gabe Finch and Kathryn Carr <kathrynelizabethcarr@gmail.com>
2015-03-25 11:55:15 -0700

    adds navbar and user/space controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
 
+  resources :users
   resources :spaces
 
 end
--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 
 describe Space do
   it {should belong_to :user}
+  it {should validate_presence_of :address}
+  it {should validate_presence_of :description}
+  it {should validate_presence_of :cost}
 end
