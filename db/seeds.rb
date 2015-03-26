4627233f1c4dbfde969504584c92a3e3dd587d2f
katcarr <kathrynelizabethcarr@gmail.com>
2015-03-26 07:48:25 -0700

    Add to db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+users = User.create!([{email: 'rogerwhite@awesome.com', password: '12345678'},
+                    {email: 'juniperruby@awesome.com', password: '12345678'}])
+
+
+
+space1 = users.first.spaces.create!({description: "A place where you can go and take off your hat.",
+                            address: "Dream Forest", cost: "112"})
+
+
+
+
+
+photos1 = users.first.spaces.first.photos.create!([{image: "app/assets/images/cabin1_1.jpg"},
+                                 {image: "app/assets/images/cabin1_2.jpg"},
+                                 {image: "app/assets/images/cabin1_3.jpg"}])
+
+
+users[1].spaces.create!([{description: "A lovely remote retreat away from it all",
+                            address: "Columbia Gorge", cost: "96"}])
+
+
+users[1].spaces.first.photos.create!([{image: "app/assets/images/cabin2_1.jpg"},
+                                 {image: "app/assets/images/cabin2_2.jpg"},
+                                 {image: "app/assets/images/cabin2_3.jpg"},
+                                 {image: "app/assets/images/cabin2_4.jpg"}])
